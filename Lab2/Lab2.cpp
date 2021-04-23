09887395d79b7525d7dbe68a446523a4d98f07b5
VEL <yrchenko644@gmail.com>
2021-04-23 21:07:06 +0300

    Created class BinaryTree

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -11,6 +11,18 @@ struct TNode {
 	TNode(int d);
 };
 
+class BinaryTree {
+public:
+	void preorder();
+	void inorder();
+	void postorder();
+protected:
+	TNode* root;
+	void preorder_rec(TNode* p);
+	void inorder_rec(TNode* p);
+	void postorder_rec(TNode* p);
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
