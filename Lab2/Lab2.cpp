4a0c160a5af97c5d896fb6114d6e3bbc49b41b1e
VEL <yrchenko644@gmail.com>
2021-04-23 21:25:10 +0300

    Created Inorder, postorder. addItem

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -18,8 +18,10 @@ public:
 	void preorder();
 	void inorder();
 	void postorder();
+	void addNode(int item);
 protected:
 	TNode* root;
+	TNode* addNode_rec(int item, TNode* p);
 	void preorder_rec(TNode* p);
 	void inorder_rec(TNode* p);
 	void postorder_rec(TNode* p);
@@ -60,8 +62,83 @@ void BinaryTree::preorder_rec(TNode* p)
 	}
 }
 
+
+void BinaryTree::inorder()
+{
+	cout << "   inorder sequence of nodes: \n";
+	if (root == NULL)
+		cout << "tree is empty\n";
+	else
+	{
+		inorder_rec(root);
+		cout << ";\n";
+	}
+}
+
+void BinaryTree::inorder_rec(TNode* p)
+{
+	if (p != NULL)
+	{
+		inorder_rec(p->left);
+		cout << ", " << p->data;
+		inorder_rec(p->right);
+	}
+}
+
+
+void BinaryTree::postorder()
+{
+	cout << "   postorder sequence of nodes: \n";
+	if (root == NULL)
+		cout << "tree is empty\n";
+	else
+	{
+		postorder_rec(root);
+		cout << ";\n";
+	}
+}
+
+void BinaryTree::postorder_rec(TNode* p)
+{
+	if (p != NULL)
+	{
+		postorder_rec(p->left);
+		postorder_rec(p->right);
+		cout << ", " << p->data;
+	}
+}
+
+void BinaryTree::addNode(int item)
+{
+	root = addNode_rec(item, root);
+}
+
+TNode* BinaryTree::addNode_rec(int item, TNode* p)
+{
+	if (p == NULL)
+	{
+		p = new TNode(item);
+		p->left = p->right = NULL;
+	}
+	else if (item < p->data)
+		p->left = addNode_rec(item, p->left);
+	else
+		p->right = addNode_rec(item, p->right);
+	
+	return p;
+}
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	BinaryTree BST;
+	BST.addNode(1);
+	BST.addNode(2);
+	BST.addNode(3);
+	BST.addNode(4);
+	BST.addNode(5);
+	BST.addNode(6);
+	BST.preorder();
+	return 0;
 }
 
